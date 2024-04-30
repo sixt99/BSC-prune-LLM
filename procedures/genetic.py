53ded924997c3559947e75488559fab9415863cf
Sixte <sixte99@gmail.com>
2024-04-30 11:31:10 +0200

    added randomly_slide_by_one_position policy

--- procedures/genetic.py
+++ procedures/genetic.py
@@ -16,120 +16,124 @@ def create_generation_csv(attempt_path):
     generation = max(folder_idxs) + 1 if folder_idxs else 0
     generation_path = attempt_path + f'/generation_{generation}.csv'
     with open(generation_path, 'w') as file:
-        heading = "eval_loss,eval_accuracy,eval_precision,eval_recall,eval_f1,eval_matthews,eval_runtime,eval_samples_per_second,eval_steps_per_second,area_percentage,block_size,real_area_percentage,pruned_layer_names,grid_shapes,pairs\n"
+        heading = "eval_loss,eval_accuracy,eval_precision,eval_recall,eval_f1,eval_matthews,eval_runtime,eval_samples_per_second,eval_steps_per_second,area_percentage,block_size,real_area_percentage,pruned_layer_names,grid_shapes,n_blocks,genes\n"
         file.write(heading)
     return generation_path
 
-def get_layer_names(model):
+def get_pruning_info(model, area_percentage, block_size):
     include_string = ".layer"
-    layer_names = [layer_name for layer_name in model.state_dict().keys() if len(model.state_dict()[layer_name].shape) == 2 and include_string in layer_name]
-    return layer_names
+    pruned_layer_names = [pruned_layer_name for pruned_layer_name in model.state_dict().keys() if len(model.state_dict()[pruned_layer_name].shape) == 2 and include_string in pruned_layer_name]
+    grid_shapes = [tuple(np.array(model.state_dict()[pruned_layer_name].shape) // block_size) for pruned_layer_name in pruned_layer_names]
+    total_n_blocks_per_layer = [grid_shape_x * grid_shape_y for grid_shape_x, grid_shape_y in grid_shapes]
+    n_blocks = [round(area_percentage * x) for x in total_n_blocks_per_layer]
+    real_area_percentage = np.sum(n_blocks) / np.sum(total_n_blocks_per_layer)
 
-def get_grid_shapes(model, layer_names, block_size):
-    grid_shapes = []
-    for layer_name in layer_names:
-        grid_shapes.append(tuple(np.array(model.state_dict()[layer_name].shape) // block_size))
-    return grid_shapes
+    assert(len(pruned_layer_names) == len(grid_shapes) == len(total_n_blocks_per_layer) == len(n_blocks))
 
-def get_real_area_percentage(pairs, block_size, grid_shapes):
-    total_area = np.sum([grid_shape[0]*grid_shape[1] for grid_shape in grid_shapes])
-    pruned_area = np.sum([len(pairs[i]) for i in range(len(pairs))])
-    return pruned_area/total_area
+    pruning_info = {}
+    pruning_info['area_percentage'] = area_percentage
+    pruning_info['block_size'] = block_size
+    pruning_info['real_area_percentage'] = real_area_percentage
+    pruning_info['pruned_layer_names'] = pruned_layer_names
+    pruning_info['grid_shapes'] = grid_shapes
+    pruning_info['n_blocks'] = n_blocks
 
-def evaluate_and_write_to_csv(trainer, tokenized_dataset, area_percentage, block_size, real_area_percentage, pruned_layer_names, grid_shapes, pairs, generation_path):
-    evaluation = trainer.evaluate(tokenized_dataset)
-    string = evaluation2string(evaluation, area_percentage, block_size, real_area_percentage, pruned_layer_names, grid_shapes, pairs)
-    with open(generation_path, 'a') as f:
-        print(string, file=f)
+    return pruning_info
 
-def evaluation2string(evaluation, area_percentage, block_size, real_area_percentage, pruned_layer_names, grid_shapes, pairs):
+def evaluation_info2string(evaluation, pruning_info, genes):
     string = ''
     for x in evaluation.keys():
         string += str(evaluation[x]) + ","
-    string += str(area_percentage) + ","
-    string += str(block_size) + ","
-    string += str(real_area_percentage) + ","
-    string += '"' + str(pruned_layer_names) + '"' + ","
-    string += '"' + str(grid_shapes) + '"' + ","
-    string += '"' + str(pairs) + '"'
+    for x in pruning_info.keys():
+        string += '"' + str(pruning_info[x]) + '",' if ',' in str(pruning_info[x]) else str(pruning_info[x]) + ","
+    string += '"' + str(genes) + '"'
     string = string.replace(' ', '')
-
     return string
 
-def string2pairs(string):
-    return [set(tuple(map(int,y.split(','))) for y in x[1:-1].split("),(")) for x in string[2:-2].split("},{")]
+def string2genes(string):
+    return list(map(int, string[1:-1].split(',')))
 
-def randomly_populate(n_individuals, area_percentage, block_size, generation_path, tokenized_dataset, layer_names, grid_shapes):
-    # Iterate as many times as needed to reach max_n_rows of data
+def randomly_prune_model_by_area(model, pruning_info):
+    random.seed(time.time())
+    genes = []
+    for layer_name, (grid_shape_x, grid_shape_y), n_blocks in zip(pruning_info['pruned_layer_names'], pruning_info['grid_shapes'], pruning_info['n_blocks']):
+        pairs = set()
+        while(len(pairs) < n_blocks):
+            i = random.randint(0, grid_shape_x - 1)
+            j = random.randint(0, grid_shape_y - 1)
+            pairs.add((i,j))
+        for i, j in list(pairs):
+            model.state_dict()[layer_name][block_size * i : block_size * (i + 1), block_size * j : block_size * (j + 1)].fill_(0)
+            genes += [i,j]
+    return genes
+    
+def randomly_populate(n_individuals, generation_path, tokenized_dataset, pruning_info):
     for _ in range(n_individuals):
-        # Start with a clean non-pruned model
         model = load_model()
         trainer = load_trainer(model)
-        pairs = []
+        genes = randomly_prune_model_by_area(model, pruning_info)
+        evaluation = trainer.evaluate(tokenized_dataset)
+        string = evaluation_info2string(evaluation, pruning_info, genes)
+        with open(generation_path, 'a') as f:
+            print(string, file=f)
 
-        # Prune all weight matrices
-        for layer in layer_names:
-            tensor = model.state_dict()[layer]
-            output = randomly_prune_blocks_by_area(tensor, area_percentage, block_size, verbose=True)
-            pairs.append(output['pairs'])
-        
-        # Compute how much area we are removing. This number should be very close to area_percentage
-        real_area_percentage = get_real_area_percentage(pairs, block_size, grid_shapes)
-    
-        # See how the pruning affects the model and write the output to the .csv file
-        evaluate_and_write_to_csv(trainer, tokenized_dataset, area_percentage, block_size, real_area_percentage, layer_names, grid_shapes, pairs, generation_path)
+def prune_model_by_genes(model, pruning_info, genes):
+    gene_count = 0
+    for layer_name, block_count in zip(pruning_info['pruned_layer_names'], pruning_info['n_blocks']):
+        for _ in range(block_count):
+            i = genes[gene_count]
+            j = genes[gene_count + 1]
+            model.state_dict()[layer_name][block_size * i : block_size * (i + 1), block_size * j : block_size * (j + 1)].fill_(0)
+            gene_count += 2
 
-def mutate(pairs, grid_shapes, mutation_probability, mutation_policy = "slide_one_position"):
-    np.random.seed(42)
-    probabilities = [mutation_probability/2,1-mutation_probability,mutation_probability/2]
-    choices = [-1, 0, 1]
-    mutated_pairs = []
+def mutate(genes, pruning_info, mutation_probability, mutation_policy = "randomly_change"):
+    random.seed(time.time())
+    mutated_genes = []
+    gene_count = 0
+    for (grid_shape_x, grid_shape_y), block_count in zip(pruning_info['grid_shapes'], pruning_info['n_blocks']):
+        pairs = set()
+        for _ in range(block_count):
+            mutate = random.random() <= mutation_probability
+            if not mutate:
+                mutated_genes.append(genes[gene_count])
+                mutated_genes.append(genes[gene_count + 1])
+            else:
+                if mutation_policy == 'randomly_change':
+                    while True:
+                        i = random.randint(0, grid_shape_x - 1)
+                        j = random.randint(0, grid_shape_y - 1)
+                        if (i,j) not in pairs and (i,j) != (genes[gene_count], genes[gene_count + 1]):
+                            pairs.add((i,j))
+                            mutated_genes += [i,j]
+                            break
+                elif mutation_policy == 'randomly_slide_by_one_position':
+                    while True:
+                        i = genes[gene_count] + random.choice([-1,0,1])
+                        j = genes[gene_count + 1] + random.choice([-1,0,1])
+                        if (i,j) not in pairs and (i,j) != (genes[gene_count], genes[gene_count + 1]):
+                            pairs.add((i,j))
+                            mutated_genes += [i,j]
+                            break
+            gene_count += 2
 
-    # Iterate over each layer and get its respective pruning
-    # Remember 'pairs' is of the form [{(4,2),(5,4),(2,5)}, {(3,2)}, {(5,3),(1,5),(0,0),(1,2)}]
-    for iter, set_pairs in enumerate(pairs):
-        aux = {} # We will store mutated pairs layer-wise here
-        grid_shape = grid_shapes[iter] # We need grid_shapes to be sure the mutation does not surpass grid limits
-        for tuple in list(set_pairs):
-            i, j = tuple
-            while True:
-                if mutation_policy == "slide_one_position":
-                    mutation_i = np.random.choice(choices, p=probabilities)
-                    mutation_j = np.random.choice(choices, p=probabilities)
-                    i = i + mutation_i if i + mutation_i in range(grid_shape[0]) else i - mutation_i
-                    j = j + mutation_j if j + mutation_j in range(grid_shape[1]) else j - mutation_j
-                # If (i,j) has not appeared so far, add it to aux and go mutate the next one
-                # If (i,j) is already in aux, mutate again
-                if not (i,j) in aux:
-                    aux.append((i,j))
-                    break
-        assert len(set_pairs) == len(aux)
-        mutated_pairs.append(aux)
-    
-    return mutated_pairs
+    return mutated_genes
 
-# TODO too many computations layer_names grid_shapes
-def prune_by_block_dictionary(model, block_dictionary, block_size):
-    for layer_name in layer_names:
-        tensor = model.state_dict()[layer_name]
-        for i, j in block_dictionary[layer_name]:
-            block = tensor[block_size * i : block_size * (i + 1), block_size * j : block_size * (j + 1)]
-            block.fill_(0)
+random.seed(time.time())
 
 # Hyperparameters
-n_generations = 50
-n_individuals = 1
-select_n_best = 4
+n_generations = 10
+n_individuals = 10
+select_n_best = 3
 area_percentage = 0.3
-block_size = 128
+block_size = 64
 metric = "eval_matthews"
+mutation_probability = 0.4
 
-# Initialize model and some info
+# Initialize model and get some information about the pruning
 model = load_model()
 tokenizer = load_tokenizer()
 tokenized_dataset = load_tokenized_data(tokenizer)['validation']
-layer_names = get_layer_names(model)
-grid_shapes = get_grid_shapes(model, layer_names, block_size)
+pruning_info = get_pruning_info(model, area_percentage, block_size)
 
 # Path definitions
 output_path = "/Users/sixteoriolllenassegura/prune_llm/procedures/genetic_outputs"
@@ -138,32 +142,32 @@ generation_path = create_generation_csv(attempt_path)
 
 # Create an initial random population of n_individuals
 print('Generation 0')
-randomly_populate(n_individuals, area_percentage, block_size, generation_path, tokenized_dataset, layer_names, grid_shapes)
+randomly_populate(n_individuals, generation_path, tokenized_dataset, pruning_info)
 
 # Iterate over the generations
 for generation in range(1, n_generations):
-    # TODO instead of read this from a file, make sure you have this in memory from the previous iteration
+    # Get the best individuals from the previous generation
     df = pd.read_csv(generation_path)
+    best_individual_idxs = np.argsort(list(df[metric]))[-select_n_best:]
+    print(f'Best metric in generation {generation - 1}:')
+    print(np.sort(list(df[metric]))[-select_n_best:])
 
-    # Go for the next iteration
+    # Start new generation by including the best parents
     print(f'Generation {generation}')
     generation_path = create_generation_csv(attempt_path)
+    selected_rows = df.loc[best_individual_idxs].to_csv(generation_path, mode='a', header=False, index=False)
 
-    # Get the best individuals from the previous generation
-    sorted_idxs = np.argsort(df[metric])
-    for idx in sorted_idxs[-select_n_best:]:
-        pairs = string2pairs(df.loc[idx]['pairs'])
-        print(df.loc(idx)[metric])
-        print(pairs)
-        exit()
-        for _ in range(3):
-            # TODO we should not mutate on mutations
-            mutate(block_dictionary, grid_shapes)
-            model = load_model()
-            trainer = load_trainer(model)
-            prune_by_block_dictionary(model, block_dictionary, block_size)
-            evaluation = trainer.evaluate(tokenized_dataset)
+    # Create a new generation from the best individuals of the last
+    for _ in range(n_individuals - select_n_best):
+        idx = random.choice(best_individual_idxs)
+        genes = string2genes(df.loc[idx, 'genes'])
+        mutated_genes = mutate(genes, pruning_info, mutation_probability)
 
-            string = global_block_pruning2string(evaluation, area_percentage, block_size, block_dictionary)
-            with open(generation_path, 'a') as f:
-                print(string, file=f)
+        model = load_model()
+        trainer = load_trainer(model)
+        prune_model_by_genes(model, pruning_info, mutated_genes)
+        evaluation = trainer.evaluate(tokenized_dataset)
+
+        string = evaluation_info2string(evaluation, pruning_info, mutated_genes)
+        with open(generation_path, 'a') as f:
+            print(string, file=f)
