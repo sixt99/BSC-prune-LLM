b6bffa8f5a23215fae0d57b0131d2e00a1aeef6f
Sixte <sixte99@gmail.com>
2024-05-30 16:41:20 +0200

    friendly data analyser

--- procedures/analyse_data.py
+++ procedures/analyse_data.py
@@ -3,47 +3,162 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils import *
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import json
+
+def string2genes(string):
+    return list(map(int, string[1:-1].split(", ")))
+
+def random_color():
+    return (random.random(), random.random(), random.random())
+
+def get_training_lines(mylist, myindex):
+    return [mylist[i-1:i+1] for i in myindex if i-1 >= 0]
+
+def get_evolve_lines(mylist, myindex):
+    return [mylist[s : e] for s, e in zip([0] + myindex, myindex + [None]) if len(mylist[s : e]) >= 2]
+
 
 #67
-attempt = 122
+#122
+#148
+#153
+#154
+#175
+#195
+#215
+# [259, 260, 261]
+# [263, 264, 265, 267] Inacabades
+# [269, 270, 271, 272, 273, 274]
+# [276, 277, 278, 279, 280]
+
 sort_by = ["eval_matthews", "pruned_area"]
-attempt_path = f"/Users/sixteoriolllenassegura/prune_llm/marenostrum_layerwise/attempt_{attempt}/"
+attempts = [269, 270, 271, 272, 273, 274]
+line = None
+dict = {}
+for attempt in attempts:
+    attempt_path = f"/Users/sixteoriolllenassegura/prune_llm/marenostrum_layerwise/attempt_{attempt}/"
+    folders = [x for x in os.listdir(attempt_path) if os.path.isdir(attempt_path + "/" + x)]
+    idxs = [int(x.split('_')[0]) for x in folders]
+    folders = [folders[x] for x in np.argsort(idxs)]
+    dict[attempt] = folders
 
-folders = [x for x in os.listdir(attempt_path) if 'transformer' in x]
-idxs = [int(x.split('_')[0]) for x in folders]
-idxs = np.argsort(idxs)
-folders = [folders[x] for x in idxs]
+colors = ['r', 'g', 'b', 'k', 'c', 'm', 'y']
 
-x_values = []
-y_values = []
+n_iterations = max([len(dict[attempt]) for attempt in attempts])
 
-for layer in folders:
-    layer_path = attempt_path + f"{layer}"
-    n_generations = len(os.listdir(layer_path)) - 1
-    for iter in range(n_generations):
-        generation_path = layer_path + f"/generation_{iter}.csv"
-        df_it = pd.read_csv(generation_path)
-        if iter == 0:
-            df = df_it
-            continue
-        df = pd.concat([df, df_it], axis = 0).reset_index(drop=True)
+x_values = {attempt : [] for attempt in attempts}
+y_values = {attempt : [] for attempt in attempts}
+x_val = {attempt : [] for attempt in attempts}
+y_val = {attempt : [] for attempt in attempts}
+training_idxs = {attempt : [] for attempt in attempts}
 
-    x = df[sort_by[0]]
-    y = df[sort_by[1]]
-    hist, x_edges, y_edges, _ = plt.hist2d(x, y, bins=200, cmap='viridis', range=[[0.4, 0.85], [0, 1]])
-    max_idx = np.argmax(hist)
-    max_idx_2d = np.unravel_index(max_idx, hist.shape)
+for iter in range(0, n_iterations):
 
-    plt.xlabel(sort_by[0])
-    plt.ylabel(sort_by[1])
-    plt.colorbar(label='Frequency')
+    if iter != 0:
+        fig, axs = plt.subplots(1, 2, figsize=(15, 7))
+        
+        axs[1].set_xlim(0.6, 1)
+        axs[1].set_ylim(0, 0.8)
+        axs[0].set_xlim(0.2, 0.6)
+        axs[0].set_ylim(0, 0.8)
+    
+    for i, attempt in enumerate(attempts):
+        attempt_path = f"/Users/sixteoriolllenassegura/prune_llm/marenostrum_layerwise/attempt_{attempt}/"
 
-    plt.title(layer)
+        # -------- TRAINING SET --------
+        max_iter = len(dict[attempt]) - 1
+        print(f"Attempt {attempt}:", dict[attempt][min(iter, max_iter)])
+        layer_path = attempt_path + f"{dict[attempt][min(iter, max_iter)]}"
 
-    x_values.append(x_edges[max_idx_2d[0]])
-    y_values.append(y_edges[max_idx_2d[1]])
+        path = layer_path + f"/configuration.json"
 
-    # Draw a line between the two points
-    plt.plot(x_values, y_values, color='red', linewidth=0.5)
+        df = None
+        if os.path.exists(path):
+            with open(path, 'r') as file:
+                best = json.load(file)['best_individual']
+                df = pd.DataFrame([best])
+                if 'train' in layer_path:
+                    training_idxs[attempt].append(iter)
 
-    plt.show()
+        if df is not None:
+            # Draw a line and keep it in the records
+            best = np.argmax(df['eval_custom'])
+            x, y = df.loc[best, sort_by[0]], df.loc[best, sort_by[1]]
+            x_values[attempt].append(x)
+            y_values[attempt].append(y)
+
+            if iter != 0:
+                # Evolve lines
+                partitions_x_evolve = get_evolve_lines(x_values[attempt], training_idxs[attempt])
+                partitions_y_evolve = get_evolve_lines(y_values[attempt], training_idxs[attempt])
+                for partition_x, partition_y in zip(partitions_x_evolve, partitions_y_evolve):
+                    axs[1].plot(partition_x, partition_y, linewidth=0.8, color = colors[attempts.index(attempt)])
+            
+                # Train lines
+                partitions_x_train = get_training_lines(x_values[attempt], training_idxs[attempt])
+                partitions_y_train = get_training_lines(y_values[attempt], training_idxs[attempt])
+                for partition_x, partition_y in zip(partitions_x_train, partitions_y_train):
+                    axs[1].plot(partition_x, partition_y, linewidth=0.8, color = colors[attempts.index(attempt)], linestyle=':')
+
+                if max_iter <= iter:
+                    axs[1].plot(x_values[attempt][-1], y_values[attempt][-1], marker='.', markersize=10, color = colors[attempts.index(attempt)])
+                    
+                axs[1].set_title('Training')
+                axs[1].set_xlabel(sort_by[0])
+        
+        # -------- VALIDATION SET --------
+        df = None
+        if os.path.exists(path):
+            with open(path, 'r') as file:
+                best = json.load(file)['best_individual_validation']
+                df = pd.DataFrame([best])
+        
+        if df is not None:
+            # Draw a line and keep it in the records
+            best = np.argmax(df['eval_custom'])
+            x, y = df.loc[best, sort_by[0]], df.loc[best, sort_by[1]]
+            x_val[attempt].append(x)
+            y_val[attempt].append(y)
+
+            if iter != 0:
+                # Evolve lines
+                partitions_x_evolve = get_evolve_lines(x_val[attempt], training_idxs[attempt])
+                partitions_y_evolve = get_evolve_lines(y_val[attempt], training_idxs[attempt])
+                k = 0
+                for partition_x, partition_y in zip(partitions_x_evolve, partitions_y_evolve):
+                    if k == 0:
+                        label = 'Attempt ' + str(attempt)
+                    else:
+                        label = None
+                    axs[0].plot(partition_x, partition_y, linewidth=0.8, color = colors[attempts.index(attempt)], label = label)
+                    k += 1
+
+
+                # Train lines
+                partitions_x_train = get_training_lines(x_val[attempt], training_idxs[attempt])
+                partitions_y_train = get_training_lines(y_val[attempt], training_idxs[attempt])
+                for partition_x, partition_y in zip(partitions_x_train, partitions_y_train):
+                    axs[0].plot(partition_x, partition_y, linewidth=0.8, color = colors[attempts.index(attempt)], linestyle=':')
+
+                if max_iter <= iter:
+                    axs[0].plot(x_val[attempt][-1], y_val[attempt][-1], marker='.', markersize=10, color = colors[attempts.index(attempt)])
+                
+                axs[0].set_title('Validation')
+                axs[0].set_xlabel(sort_by[0])
+                axs[0].set_ylabel(sort_by[1])
+                axs[0].legend()
+        
+    print('--------------')
+
+    path = f"pics/{str(attempts).replace(', ', '_').replace('[', '').replace(']', '')}"
+    if not os.path.exists(path):
+        os.mkdir(path)
+    
+    if iter != 0:
+        plt.tight_layout()
+        plt.show()
+        #plt.savefig(path + f'/iter_{iter}')
